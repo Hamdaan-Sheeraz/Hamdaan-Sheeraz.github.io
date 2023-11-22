4f75bd1df0e3029c6925e858f6130839ce46b351
Hamdaan-Sheeraz <151772637+Hamdaan-Sheeraz@users.noreply.github.com>
2023-11-22 21:22:07 +0400

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,8 @@
+// Clicking Login
+let login_button = document.querySelector('.log');
 
+function redirectToLog() {
+    window.location.href = './loginQ.html'
+}
+
+login_button.onclick = redirectToLog;
